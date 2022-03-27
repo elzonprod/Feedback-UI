33350b3ea157df1ce459207c0e857f7d6e053746
Dmitriy <dmitriy.timoshchenko@gmail.com>
2022-03-27 12:39:15 +0200

    Animation on loading

--- src/components/FeedbackList.jsx
+++ src/components/FeedbackList.jsx
@@ -2,11 +2,18 @@ import FeedbackItem from "./FeedbackItem";
 import { motion, AnimatePresence } from "framer-motion";
 import FeedbackContext from "../context/FeedbackContext";
 import { useContext } from "react";
+import Spinner from "./shared/Spinner";
 
 const FeedbackList = () => {
-  const { feedback } = useContext(FeedbackContext);
+  const { feedback, isLoading } = useContext(FeedbackContext);
 
-  return (
+  if (!isLoading && (!feedback || feedback.length === 0)) {
+    return <p>No Feedback Yet</p>;
+  }
+
+  return isLoading ? (
+    <Spinner />
+  ) : (
     <div className="feedback-list">
       <AnimatePresence>
         {feedback.map((item) => (
